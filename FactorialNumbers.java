8cfae70625064a377a49dbab9cb9733a7c339ebf
sequeloble <andrephelipe@outlook.com>
2019-12-30 20:57:28 -0300

    Make more of loop exercises

--- FactorialNumbers.java
+++ FactorialNumbers.java
@@ -1,11 +1,13 @@
 class FactorialNumbers {
     public static void main(String[] args) {
         int i = 0;
-        int fact = 0;
+        int fact = 1;
 
-        while (i <= 10) {
+        while (i < 10) {
+            i++;
             fact = fact * i;
-            i++; 
+            System.out.println(fact);
         }
+        
     }
 }

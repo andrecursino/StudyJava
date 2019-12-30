04f194b669625aff19bfd95f27a742f3df5d63c5
Andre Cursino <andre.cursino@winsrv01.net>
2019-12-30 14:00:40 -0300

    start the code

---
+++ LoopExercises/FactorialNumbers.java
@@ -0,0 +1,11 @@
+class FactorialNumbers {
+    public static void main(String[] args) {
+        int i = 0;
+        int fact = 0;
+
+        while (i <= 10) {
+            fact = fact * i;
+            i++; 
+        }
+    }
+}

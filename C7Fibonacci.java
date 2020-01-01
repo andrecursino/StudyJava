6905cd2eb79ef6a06b9207c9bbb60a62580a6524
sequeloble <andrephelipe@outlook.com>
2019-12-31 21:36:05 -0300

    finished Fibonacci algorithm

--- C7Fibonacci.java
+++ C7Fibonacci.java
@@ -1,7 +1,14 @@
 class C7Fibonacci {
     public static void main(String[] args) {
-            int fibo = 1
-            int n1 = fibo;
-            int n2 = 
+            int fibo = 0;
+            int next1 = 0;
+            int previous = 1;
+            
+            while (previous <= 100) {
+                next1 = fibo + previous;
+                System.out.println(next1);
+                previous = fibo;
+                fibo = next1;
+            }
     }
 }

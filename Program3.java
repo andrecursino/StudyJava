f46b9cb90deb08e47a96aaa5e41efab7897ff637
sequeloble <andrephelipe@outlook.com>
2020-01-03 18:19:18 -0300

    Implemented the classes and called a object for tests

--- Program3.java
+++ Program3.java
@@ -6,18 +6,41 @@ class Account {
     String openingDate;
 
     void withdrawMoney(double quantity) {
-
+        if(this.balance < quantity) {
+            System.out.println("Unauthorized Operation\n" +
+            "Your balance is not suffieciente");
+        } else {
+            this.balance -= quantity;
+        }
     }
 
     void cashDeposit(double quantity) {
-
+        this.balance += quantity;
     }
 
     double yieldProfit() {
-
+        double profit = this.balance * 0.1;
+        return profit;
     }
 }
 
 class Program3 {
-    
+    public static void main(String[] args) {
+        Account myAccount = new Account();
+        myAccount.balance = 300;
+        myAccount.yieldProfit();
+        
+        System.out.println(myAccount.yieldProfit());
+
+        Account c1 = new Account();
+        c1.ownerAccount = "Hugo";
+        c1.numberAccount = 123;
+        c1.agencyAccount = "4567-7";
+        c1.balance = 50;
+        c1.openingDate = "16/04/2017";
+
+        c1.cashDeposit(100);
+        System.out.println("Current balance: " + c1.balance);
+        System.out.println("Monthly earnings: " + c1.yieldProfit());
+    }
 }

6bcc77d9caa89867539fb601dd54802bd10d8d26
sequeloble <andrephelipe@outlook.com>
2020-01-02 22:50:04 -0300

    Created class Account

---
+++ Program3.java
@@ -0,0 +1,23 @@
+class Account {
+    String ownerAccount;
+    int numberAccount;
+    String agencyAccount;
+    double balance;
+    String openingDate;
+
+    void withdrawMoney(double quantity) {
+
+    }
+
+    void cashDeposit(double quantity) {
+
+    }
+
+    double yieldProfit() {
+
+    }
+}
+
+class Program3 {
+    
+}
